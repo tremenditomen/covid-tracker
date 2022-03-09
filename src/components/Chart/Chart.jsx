6088e8d93deb7b51da960ccc499801a6ab93acbf
Luis Andres Contreras <tremenditomen@gmail.com>
2022-03-09 12:21:55 -0700

    got the chart up but its not displaying the data bug fixing required

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
-function Chart() {
-  return (
-    <div>Chart</div>
-  )
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+const Chart = () => {
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    fetchAPI();
+  }, []);
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        label: dailyData.map(({date}) =>date),
+        datasets: [{
+          data:dailyData.map(({confirmed}) => confirmed),
+          label : 'Infected',
+          borderColor : '#3333ff',
+          fill: true
+        }, {
+          data:dailyData.map(({deaths}) => deaths),
+          label : 'Deaths',
+          borderColor : 'red',
+          backgroundColor : 'rgba(255,0,0,0.5)',
+          fill: true
+        }],
+      }}
+    />
+  ) : null;
+  return <div className={styles.container}>{lineChart}</div>;
 }
 
-export default Chart
+export default Chart;
