c3a65b2048001827162a9406c8c948c98be1f4fe
Luis Andres Contreras <tremenditomen@gmail.com>
2022-03-10 16:21:31 -0700

    fixed the charts :D

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -24,32 +24,59 @@ ChartJS.register(
 );
 const Chart = () => {
   const [dailyData, setDailyData] = useState([]);
-
   useEffect(() => {
     const fetchAPI = async () => {
       setDailyData(await fetchDailyData());
     };
     fetchAPI();
   }, []);
-  const lineChart = dailyData.length ? (
-    <Line
-      data={{
-        label: dailyData.map(({date}) =>date),
-        datasets: [{
-          data:dailyData.map(({confirmed}) => confirmed),
-          label : 'Infected',
-          borderColor : '#3333ff',
-          fill: true
-        }, {
-          data:dailyData.map(({deaths}) => deaths),
-          label : 'Deaths',
-          borderColor : 'red',
-          backgroundColor : 'rgba(255,0,0,0.5)',
-          fill: true
-        }],
-      }}
+
+  // const data = {
+  //   labels: ['January', 'February', 'March',
+  //          'April', 'May'],
+  // datasets: [
+  //   {
+  //     label: 'Rainfall',
+  //     fill: false,
+  //     lineTension: 0.5,
+  //     backgroundColor: 'rgba(75,192,192,1)',
+  //     borderColor: 'rgba(0,0,0,1)',
+  //     borderWidth: 2,
+  //     data: [65, 59, 80, 81, 56]
+  //   }
+  // ]
+  // }
+
+  const data = {
+    labels: dailyData?.map(({date}) => new Date(date).toLocaleDateString()),
+    datasets: [
+      {
+        label : 'Infected',
+        borderColor : '#3333ff',
+        fill: true,
+        backgroundColor: 'rgba(75,192,192,1)',
+        data:dailyData?.map((data) => data.confirmed)
+
+      },
+    
+      {
+        label : 'Deaths',
+        borderColor : 'red',
+        fill: true,
+        backgroundColor: 'rgba(255,0,0,0.5)',
+        data:dailyData?.map((data) => data.deaths)
+
+      }
+    ]
+  }
+
+  const lineChart = dailyData[0] ? (
+    <Line    
+    data={data}
+    
     />
   ) : null;
+
   return <div className={styles.container}>{lineChart}</div>;
 }
 
